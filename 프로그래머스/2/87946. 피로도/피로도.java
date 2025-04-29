4170da49a6e0a756cd7b3247fa613081e8c65719
eunjung <42036131+enjung@users.noreply.github.com>
2025-04-30 01:30:01 +0900

    [level 2] Title: 피로도, Time: 3.04 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    static int answer = 0;
+    int[] visited;
+    
+    public int solution(int k, int[][] dungeons) {
+        visited = new int[dungeons.length];
+        dfs(k,dungeons, 0);
+        return answer;
+    }
+    
+    
+    public void dfs(int k, int[][] dungeons, int depth){
+        answer = Math.max(answer,depth);
+        
+        for(int i=0;i<dungeons.length;i++){
+            if(visited[i]==0 && k>=dungeons[i][0]){
+                visited[i]=1;
+                dfs(k-dungeons[i][1], dungeons,depth+1);
+                visited[i]=0; //백트래킹
+            }
+        }
+        
+    }
+}
