1a1781682b34454ce9aa0639c0cd0890d25ea01f
eunjung <42036131+enjung@users.noreply.github.com>
2025-06-24 19:55:36 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,20 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String,Integer> map = new HashMap<>();
         int answer = 1;
-        for( String[] cloth : clothes ){
-            if(map.containsKey(cloth[1])){
-                map.replace(cloth[1],map.get(cloth[1])+1);
-            }
-            else{
-                map.put(cloth[1],1);
-            }
+        HashMap<String,Integer> hm = new HashMap<>();
+        for(int i=0;i<clothes.length;i++){
+            hm.put(clothes[i][1], hm.getOrDefault(clothes[i][1],0) +1 );
         }
-        for( String key : map.keySet()){
-            answer *= map.get(key)+1;
+        for(String kind : hm.keySet() ){
+            answer *= (hm.get(kind)+1);
         }
-        
         return answer-1;
     }
 }
