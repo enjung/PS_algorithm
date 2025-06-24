38733d381846dd0d91b57dd091458642289ca740
eunjung <42036131+enjung@users.noreply.github.com>
2025-06-24 17:28:39 +0900

    [level 2] Title: 전화번호 목록, Time: 363.66 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,23 +2,13 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Set<String> set = new HashSet<>();
-        
-        //모든 전화번호를 Set에 넣기
-        for (String number : phone_book) {
-            set.add(number);
-        }
-
-        //각 전화번호의 접두사 prefix를 하나씩 만들어보며 검색
-        for (String number : phone_book) {
-            for (int i = 1; i < number.length(); i++) {
-                String prefix = number.substring(0, i);
-                if (set.contains(prefix)) {
-                    return false;
-                }
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        for(int i=0;i<phone_book.length-1;i++){
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                return false;
             }
         }
-
-        return true;
+        return answer;
     }
 }
