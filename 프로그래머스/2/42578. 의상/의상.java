eeead78b55d2eefc87d96a75ead2feb79dcb057f
eunjung <42036131+enjung@users.noreply.github.com>
2025-06-26 22:53:33 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,13 +3,31 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        HashMap<String,Integer> hm = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         for(int i=0;i<clothes.length;i++){
-            hm.put(clothes[i][1], hm.getOrDefault(clothes[i][1],0) +1 );
+            if(map.containsKey(clothes[i][1])){
+                map.replace(clothes[i][1], map.get(clothes[i][1])+1 );
+            }
+            else{
+                map.put(clothes[i][1],1);
+            }
         }
-        for(String kind : hm.keySet() ){
-            answer *= (hm.get(kind)+1);
+        for(String type : map.keySet()){
+            answer *= (map.get(type)+1);
         }
         return answer-1;
     }
 }
+// class Solution {
+//     public int solution(String[][] clothes) {
+//         int answer = 1;
+//         HashMap<String, Integer> map = new HashMap<>();
+//         for(int i=0;i<clothes.length;i++){
+//             map.put(clothes[i][1], map.getOrDefault(map.get(clothes[i][1]),0)+1);
+//         }
+//         for(String type : map.keySet()){
+//             answer *= (map.get(type)+1);
+//         }
+//         return answer-1;
+//     }
+// }
