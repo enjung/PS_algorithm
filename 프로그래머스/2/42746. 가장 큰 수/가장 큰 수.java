0f73e73458426156d85d7e2a67b016402ad8bd06
eunjung <42036131+enjung@users.noreply.github.com>
2025-07-04 15:28:21 +0900

    [level 2] Title: 가장 큰 수, Time: 253.27 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,29 +1,20 @@
 import java.util.*;
+
 class Solution {
     public String solution(int[] numbers) {
+        String[] strNum = new String[numbers.length];
+        for(int i=0;i<numbers.length;i++){
+            strNum[i] = String.valueOf(numbers[i]);
+        }
+        Arrays.sort(strNum, (o1,o2)->(o2+o1).compareTo(o1+o2));
         
-        // 1. 숫자 배열을 문자열 배열로 변환
-        String[] strNums = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            strNums[i] = String.valueOf(numbers[i]);
-            //int -> String , String.valueOf()사용
-        }
-
-        // 2. 정렬: 큰 조합이 앞에 오도록
-        Arrays.sort(strNums, (a, b) -> (b + a).compareTo(a + b));
-
-        // 3. 예외 처리: 000 같은 경우 → "0"
-        if (strNums[0].equals("0")) {
-            return "0";
-        }
-
-        // 4. 문자열 합치기
-        //+연산자 써서 붙이는 것보다 StringBuilder와 append를 쓰는 게 더 효율적
+        if(strNum[0].equals("0")) return "0";
+        
         StringBuilder sb = new StringBuilder();
-        for (String s : strNums) {
+        for(String s : strNum){
             sb.append(s);
         }
-        
         return sb.toString();
+        
     }
 }
