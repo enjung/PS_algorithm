c21f90f592aaa8ca657dedeac8f941ba4a64f608
eunjung <42036131+enjung@users.noreply.github.com>
2025-07-10 19:40:29 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.90 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,54 +1,57 @@
 import java.util.*;
 
 class Solution {
-    static int answer = Integer.MAX_VALUE;
-    static List<List<Integer>> graph;
-    static int[] visited;
+    int answer = Integer.MAX_VALUE;
+    List<List<Integer>> graph = new ArrayList<>();
+    int[] visited;
     
     public int solution(int n, int[][] wires) {
+        visited = new int[n+1]; //1번 인덱스부터 쓸거니까 n+1 사이즈
         
-        //그래프를 인접리스트에 저장, List<List<Integer>>로 구현
-        graph = new ArrayList<>();
-        //각 노드 연결리스트 초기화
         for(int i=0;i<=n;i++){
             graph.add(new ArrayList<>());
         }
+        
         //연결정보 저장
-        for(int[] wire : wires){
+        for(int wire[] : wires){
             int a = wire[0];
             int b = wire[1];
-            graph.get(a).add(b);
-            graph.get(b).add(a); //양방향 그래프이므로
+            graph.get(a).add(b); //a 노드의 인접 리스트 중 b를 설정
+            graph.get(b).add(a); //b 노드의 인접 리스트 중 a를 설정
         }
         
-        //전선 하나씩 끊어보면서 차이 최소인지 체크
-        for(int[] wire: wires){
+        
+        //하나씩 끊어보기
+        for(int wire[] : wires){
             int a = wire[0];
             int b = wire[1];
-            
-            //remove(b)는 인덱스b 위치의 값을 제거하는것, b를 제거하고싶으면 Object형으로 넣어줘야함
             graph.get(a).remove((Integer)b);
             graph.get(b).remove((Integer)a);
             
+            //끊을때마다 visited 초기화
             visited = new int[n+1];
-            int cnt = dfs(1); //1번 노드에서 시작
-            int dif = Math.abs(cnt-(n-cnt));
-            answer = Math.min(answer,dif);
+            //1번 노드에서 dfs 시작
+            int cnt = dfs(1);
+            //두 네트워크 크기 업데이트
+            int diff = Math.abs(cnt - (n-cnt));
+            answer = Math.min(answer,diff);
+            
+            //끊은 전선 다시 복구 (=백트래킹?)
             graph.get(a).add(b);
             graph.get(b).add(a);
+            
         }
         return answer;
     }
     
-    public int dfs(int node){
-        visited[node] = 1;
-        int cnt = 1;
-        
-        for(int n : graph.get(node)){
-            if(visited[n]==0){
-                cnt += dfs(n);
+    public int dfs(int k){
+        visited[k]=1;
+        int count = 1;
+        for (int next : graph.get(k)) {
+            if (visited[next] == 0) {
+                count += dfs(next);
             }
         }
-        return cnt;
+        return count;
     }
 }
