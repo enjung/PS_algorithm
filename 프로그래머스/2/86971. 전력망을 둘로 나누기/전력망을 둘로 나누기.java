2e2a5856b401dcdb10fc2e26a7f1628a12e3363b
eunjung <42036131+enjung@users.noreply.github.com>
2025-07-11 16:16:50 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.55 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -6,50 +6,51 @@ class Solution {
     int[] visited;
     
     public int solution(int n, int[][] wires) {
-        visited = new int[n+1]; //1번 인덱스부터 쓸거니까 n+1 사이즈
         
         for(int i=0;i<=n;i++){
             graph.add(new ArrayList<>());
         }
         
-        //연결정보 저장
-        for(int wire[] : wires){
+        visited = new int[n+1];
+        
+        //연결정보
+        for(int[] wire: wires){
             int a = wire[0];
             int b = wire[1];
-            graph.get(a).add(b); //a 노드의 인접 리스트 중 b를 설정
-            graph.get(b).add(a); //b 노드의 인접 리스트 중 a를 설정
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
-        
-        
+    
         //하나씩 끊어보기
-        for(int wire[] : wires){
+        for(int[] wire: wires){
             int a = wire[0];
             int b = wire[1];
             graph.get(a).remove((Integer)b);
             graph.get(b).remove((Integer)a);
             
-            //끊을때마다 visited 초기화
-            visited = new int[n+1];
-            //1번 노드에서 dfs 시작
-            int cnt = dfs(1);
-            //두 네트워크 크기 업데이트
-            int diff = Math.abs(cnt - (n-cnt));
-            answer = Math.min(answer,diff);
             
-            //끊은 전선 다시 복구 (=백트래킹?)
+            visited = new int[n+1];
+            //dfs로 덩어리 하나 사이즈 구하기
+            int cnt = dfs(1); //1번노드부터 시작한 사이즈
+            
+            //정답 업데이트
+            int dif= Math.abs(cnt-(n-cnt));
+            answer = Math.min(answer,dif);
+            
+            //끊은 전선 복구
             graph.get(a).add(b);
             graph.get(b).add(a);
-            
         }
         return answer;
     }
     
     public int dfs(int k){
-        visited[k]=1;
         int count = 1;
-        for (int next : graph.get(k)) {
-            if (visited[next] == 0) {
-                count += dfs(next);
+        visited[k]=1;
+        
+        for(int node : graph.get(k)){
+            if(visited[node]==0){
+                count += dfs(node);
             }
         }
         return count;
