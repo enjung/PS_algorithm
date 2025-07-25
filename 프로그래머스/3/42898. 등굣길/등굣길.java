697fa5740fe69eddd71dba97d871418bf92fc182
eunjung <42036131+enjung@users.noreply.github.com>
2025-07-25 22:38:12 +0900

    [level 3] Title: 등굣길, Time: 2.49 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -2,33 +2,41 @@ import java.util.*;
 
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[n+1][m+1];
+        int[][]dp = new int[n+1][m+1];
+        //dp[1][1]=1;
+        
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(i==1 && j==1) {
-                    dp[1][1]=1; //초기 설정 
+                if(i==1&&j==1){
+                    dp[1][1]=1;
                     continue;
                 }
+                
+                //웅덩이칸인지 검사
                 boolean isPuddle = false;
-                //해당 칸이 웅덩이 칸인지 검사
-                for(int[] p : puddles){
-                    if(p[0]==j && p[1]==i) { //행, 열 주의
+                for(int[] puddle : puddles){
+                    if(puddle[0]==j && puddle[1]==i){
                         isPuddle = true;
                         break;
                     }
                 }
-                
-                //웅덩이 칸이라면
-                if(isPuddle) {
+                //웅덩이 칸이면,
+                if(isPuddle){
                     dp[i][j]=0;
                     continue;
                 }
+                
                 int ways = 0;
-                //위에서 내려올 수 있다면
-                if(i>1) ways += dp[i-1][j];
-                //왼쪽에서 올 수 있다면
-                if(j>1) ways += dp[i][j-1];
-                dp[i][j]=ways % 1000000007;
+                //아래로 이동한 경우
+                if(i>1){
+                    ways += dp[i-1][j];
+                }
+                //오른쪽으로 이동한 경우
+                if(j>1){
+                    ways += dp[i][j-1];
+                }
+                
+                dp[i][j] = ways % 1000000007;
             }
         }
         return dp[n][m];
