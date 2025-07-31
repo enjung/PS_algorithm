9a6dabd0e88bc43dec18c01df6fabc769715e8f5
eunjung <42036131+enjung@users.noreply.github.com>
2025-07-31 22:19:21 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,28 @@
+import java.util.*;
+
 class Solution {
-    int[] visited = new int[200];
-    
-    public void dfs(int i,int[][] computers){
-        visited[i]=1;
-        
-        for(int j=0;j<computers.length;j++){
-            if(visited[j]==0 && computers[i][j]==1){
-                dfs(j,computers);
-            }
-        }
-    
-    }
-    
     public int solution(int n, int[][] computers) {
+        
+        boolean visited[] = new boolean[n];
         int answer = 0;
         
         for(int i=0;i<n;i++){
-            if(visited[i]==0){
-                dfs(i,computers);
-                answer++;
+            if(!visited[i]){
+                dfs(computers,visited,n,i);
+                answer++; 
+                //dfs안에서 answer++하는게 아니라, dfs가 끝나야 하나의 덩어리가 끝난것이므로 dfs 호출 끝나면 증가
             }
         }
         return answer;
+    }
+    
+    public void dfs(int[][] computers, boolean[] visited,int n, int i){
+        //dfs호출 조건에 방문여부를 이미 확인했으므로 바로 방문함으로 체크
+        visited[i]=true;
+        for(int j=0;j<n;j++){
+            if(!visited[j]&&computers[i][j]==1){
+                dfs(computers,visited,n,j);
+            }
+        }
     }
 }
