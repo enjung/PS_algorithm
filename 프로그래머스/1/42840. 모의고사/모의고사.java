020f65734ef87c7f18fefc6f2a40d9a650b5de66
eun9 <42036131+enjung@users.noreply.github.com>
2025-09-19 00:23:37 +0900

    [level 1] Title: 모의고사, Time: 0.97 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,32 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        List<Integer> answer = new ArrayList<>();
         int[] first = {1,2,3,4,5};
         int[] second = {2,1,2,3,2,4,2,5};
         int[] third = {3,3,1,1,2,2,4,4,5,5};
-        int[] score = new int[3];
-        int fi = 0;
-        int si = 0;
-        int ti = 0;
-        for(int n : answers){
-            if(fi>=5) fi=0;
-            if(si>=8) si=0;
-            if(ti>=10) ti=0;
-            
-            if(first[fi]==n) score[0]++;
-            if(second[si]==n) score[1]++;
-            if(third[ti]==n) score[2]++;
-            
-            fi++;
-            si++;
-            ti++;
+        int[] correct = new int[3];
+        for(int i=0;i<answers.length;i++){
+            if(answers[i]==first[i%5])correct[0]++;
+            if(answers[i]==second[i%8])correct[1]++;
+            if(answers[i]==third[i%10])correct[2]++;
         }
-        int max = Math.max(score[0],Math.max(score[1],score[2]));
-        if(score[0]==max) answer.add(1);
-        if(score[1]==max) answer.add(2);
-        if(score[2]==max) answer.add(3);
-
-        return answer.stream().mapToInt(i -> i).toArray();
+        int maxCnt = Math.max(correct[0],Math.max(correct[1],correct[2]));
+        
+        // 
+        List<Integer> ls = new ArrayList<>();
+        
+        for(int i=0;i<3;i++){
+            if(maxCnt==correct[i]) ls.add(i+1);
+        }
+        
+        int[] answer = new int[ls.size()];
+        for(int i=0;i<ls.size();i++){
+            answer[i]=ls.get(i);
+        }
+        return answer;
     }
 }
