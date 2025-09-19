4fe8e32426abcb6f9a1f4d7d1131690a70bfda51
eun9 <42036131+enjung@users.noreply.github.com>
2025-09-19 23:59:31 +0900

    [level 2] Title: 소수 찾기, Time: 49.19 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,41 +2,39 @@ import java.util.*;
 
 class Solution {
     Set<Integer> numbersSet = new HashSet<>();
-
     public int solution(String numbers) {
-        for (int len = 1; len <= numbers.length(); len++) {
-            permutation(numbers, "", len, new boolean[numbers.length()]);
+        int answer = 0;
+        
+        for(int i=1;i<=numbers.length();i++){
+            boolean[] visited = new boolean[numbers.length()];
+            permutation(numbers,"",i,visited); //길이가 i인 순열
         }
-
-        int count = 0;
-        for (int num : numbersSet) {
-            if (isPrime(num)) count++;
+        
+        for(int num : numbersSet){
+            if(isPrime(num)) answer++;
         }
-
-        return count;
+        return answer;
     }
-
-    // 순열 생성 함수 (백트래킹 방식)
-    public void permutation(String numbers, String current, int targetLength, boolean[] visited) {
-        if (current.length() == targetLength) {
-            numbersSet.add(Integer.parseInt(current));
+    
+    public void permutation(String numbers, String cur, int length, boolean[] visited){
+        if(cur.length()==length){
+            numbersSet.add(Integer.parseInt(cur));
             return;
         }
-
-        for (int i = 0; i < numbers.length(); i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                permutation(numbers, current + numbers.charAt(i), targetLength, visited);
-                visited[i] = false; // 백트래킹
+        for(int i=0;i<numbers.length();i++){
+            if(!visited[i]){
+                visited[i]=true;
+                permutation(numbers,cur+numbers.charAt(i),length,visited);
+                visited[i]=false;
             }
         }
+        
     }
-
-    // 소수 판별 함수
-    public boolean isPrime(int n) {
-        if (n <= 1) return false;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
+    
+    public boolean isPrime(int n){
+        if (n<=1) return false;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0) return false;
         }
         return true;
     }
