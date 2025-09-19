cfec86204886c8d942902886c9cd827d79299a4a
eun9 <42036131+enjung@users.noreply.github.com>
2025-09-19 16:02:45 +0900

    [level 2] Title: 카펫, Time: 0.10 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -3,18 +3,16 @@ import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        for(int w=3;w<=yellow+2;w++){
-            if((brown+yellow)%w!=0) continue;
-            int h= (brown+yellow)/w;
-            if(w<h) continue;
-            if(brown == 2*(w+h)-4){
-                if(yellow==(w-2)*(h-2)){
-                    answer[0]=w;
-                    answer[1]=h;
-                    break;
-                }
-            }
+        for(int i=1;i<=Math.sqrt(brown+yellow);i++){
+            int h = i;
+            int w = (brown+4)/2 - h;
+            if(yellow == (h-2)*(w-2)){
+                answer[0]=w;
+                answer[1]=h;
+                break;
+            } 
         }
+        
         return answer;
     }
 }
