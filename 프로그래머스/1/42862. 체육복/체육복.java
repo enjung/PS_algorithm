e04a8b861c7a4c6f9a6dc68f507b558cc42be7db
eun9 <42036131+enjung@users.noreply.github.com>
2025-09-21 16:22:41 +0900

    [level 1] Title: 체육복, Time: 0.56 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -4,64 +4,34 @@ class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         Arrays.sort(lost);
         Arrays.sort(reserve);
-        
-        int cnt = 0;
-        int[] check = new int[n+1];
-        
-        //여분 체육복이 있는 학생 check배열 1로 설정
-        for(int i=0;i<reserve.length;i++){
-            int idx=reserve[i];
-            check[idx]=1;
+        int answer = 0;
+        int[] clothes = new int[n+2];
+        Arrays.fill(clothes,1);
+        for(int r : reserve){
+            clothes[r]++;
         }
-        
-        //여분 체육복 있는 학생이 도난당한 경우 자신 여벌 체육복을 입으므로 0으로 제거
-        for(int i=0;i<lost.length;i++){
-            for(int j=0;j<reserve.length;j++){
-                if(lost[i]==reserve[j]){
-                    check[reserve[j]]=0;
-                    lost[i]=0;
-                    reserve[j]=0;
-                    break;
+        for(int l : lost){
+            clothes[l]--;
+        }
+        for(int i=1;i<=n;i++){
+            if(clothes[i]==0){
+                if(clothes[i-1]==2){
+                    clothes[i-1]=1;
+                    clothes[i]=1;
+                }   
+                else if(clothes[i+1]==2){
+                    clothes[i+1]=1;
+                    clothes[i]=1;
                 }
             }
-        }
-        
-        //도난 당한 학생 양 옆 학생이 여분 체육복이 있다면,
-        for(int i=0;i<lost.length;i++){
-            if(lost[i]==0) continue;
-            int idx = lost[i];
-            
-            //왼쪽 학생 우선
-            if(idx>1 && check[idx-1]==1){
-                check[idx-1]=0;
-                for(int j=0;j<reserve.length;j++){
-                    if(reserve[j]==idx-1){
-                        reserve[j]=0;
-                        break;
-                    }
-                }
-                lost[i]=0;
-                cnt++;
-            
-            //오른쪽 학생
-            } else if(idx < n && check[idx + 1] == 1){
-                check[idx+1]=0;
-                for(int j=0;j<reserve.length;j++){
-                    if(reserve[j]==idx+1){
-                        reserve[j]=0;
-                        break;
-                    }
-                }
-                lost[i]=0;
-                cnt++;
-            }
             
         }
         
-        int lostnwear=0;
-        for(int i=0;i<lost.length;i++){
-            if(lost[i]==0) lostnwear++;
+        for(int i=1;i<=n;i++){
+            if(clothes[i]!=0) answer++;
         }
-        return n-lost.length+lostnwear;
+    
+        return answer;
+        
     }
 }
