a7327b4cece0364ae72ca0c9d0221e6bd17bc721
eun9 <42036131+enjung@users.noreply.github.com>
2025-09-21 17:05:03 +0900

    [level 2] Title: 조이스틱, Time: 0.07 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
-메모리: 92.4 MB, 시간: 0.06 ms
+메모리: 83.9 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 25일 00:13:10
+2025년 09월 21일 17:05:01
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,32 +1,30 @@
-import java.util.*;
-
 class Solution {
     public int solution(String name) {
         int n = name.length();
-        int upDown = 0;
+        int ud = 0;
+        int cur = 0;
+        int minCur = Integer.MAX_VALUE;
         
-        //상하 조작 횟수
-        for(int i=0; i<n; i++){
+        for(int i=0;i<n;i++){
             char c = name.charAt(i);
-            upDown += Math.min(c-'A', 'Z'-c+1);
+            ud += Math.min(c-'A','Z'-c+1);
         }
+        //반환점 없는 경우
+        int noSwitch = (n-1);
         
-        //첫 번째 케이스, 방향 전환 없는 좌우 조작 횟수
-        int minMove = n-1;
-        
-        //나머지 두가지 케이스
+        //반환점 설정
         for(int i=0;i<n;i++){
             int next = i+1;
-            while(next<n && name.charAt(next)=='A') next++;
-            // 이제 next는 A블럭이 끝난 다음 상하조작이 필요한 위치
+            while( next<n && name.charAt(next)=='A') next++;
             
-            int noSwitch = n-1;
-            int forBack = 2*i + (n-next); //두 번째 케이스, 정->역
-            int backFor = i + 2*(n-next); //세 번째 케이스, 역->정
+            int forBack = i+i+n-next;
+            int backFor = n-next+n-next+i;
+            int m = Math.min(forBack,backFor);
+            cur = Math.min(m,noSwitch);
+            minCur = Math.min(cur,minCur);
             
-            minMove = Math.min(minMove,Math.min(noSwitch, Math.min(forBack,backFor)));
         }
         
-        return upDown+minMove;
+        return minCur+ud;
     }
 }
