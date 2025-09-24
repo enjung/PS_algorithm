4faf5fb7fa9de5328e4fe4786db2c2a74ee4cdc1
eun9 <42036131+enjung@users.noreply.github.com>
2025-09-24 18:14:02 +0900

    [level 2] Title: 구명보트, Time: 13.11 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,25 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        int boat = 0;
         Arrays.sort(people);
+        int answer = 0;
         int left = 0;
         int right = people.length-1;
         while(left<=right){
-            if(left==right) {
-                boat++;
-                break;
-            }
-            int sum = people[left]+people[right];
-            if(sum<=limit){
+            if(people[left]+people[right]<=limit){
                 right--;
                 left++;
             }
             else{
                 right--;
             }
-            boat++;
+            answer++;
         }
-        return boat;
+        return answer;
     }
 }
