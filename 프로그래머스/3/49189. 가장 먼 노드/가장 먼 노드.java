776832b9afb2c415446bcf8b0e3248e2839e5164
eun9 <42036131+enjung@users.noreply.github.com>
2025-09-24 17:12:53 +0900

    [level 3] Title: 가장 먼 노드, Time: 37.00 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,41 +2,36 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i=0;i<=n;i++){
-            graph.add(new ArrayList<>());
-        }
-        
-        boolean [] visited = new boolean[n+1];
         int[] dis = new int[n+1];
-        
-        for(int[] e : edge){
+        boolean[] visited = new boolean[n+1];
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0;i<=n;i++) graph.add(new ArrayList<>());
+        for(int[] e:edge){
             graph.get(e[0]).add(e[1]);
             graph.get(e[1]).add(e[0]);
         }
-        
         Queue<Integer> q = new LinkedList<>();
         q.offer(1);
-        visited[1]=true;
+        visited[1] = true;
         while(!q.isEmpty()){
-            int cur = q.poll();
-            for(int next : graph.get(cur)){
+            int node = q.remove();
+            for(int next : graph.get(node)){
                 if(!visited[next]){
-                    visited[next]=true;
-                    dis[next] = dis[cur]+1;
+                    visited[next] = true;
+                    dis[next] = dis[node]+1;
                     q.offer(next);
-                }
+                } 
             }
         }
         int max = 0;
-        for(int d : dis){
-            if(d>max) max=d;
+        for(int i=1;i<=n;i++){
+            if(max<dis[i]) max=dis[i];
+        }
+        int cnt = 0;
+        for(int i=1;i<=n;i++){
+            if(dis[i]==max) cnt++;
         }
         
-        int answer = 0;
-        for(int i=1;i<=n;i++){
-            if(dis[i]==max) answer++;
-        }
-        return answer;
+        return cnt;
     }
 }
