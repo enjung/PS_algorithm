6125db74cdc51e2dd2cf15f5dae329627be1555e
eun9 <42036131+enjung@users.noreply.github.com>
2025-09-24 16:15:15 +0900

    [level 3] Title: 입국심사, Time: 99.81 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -4,22 +4,24 @@ class Solution {
     public long solution(int n, int[] times) {
         Arrays.sort(times);
         long answer = 0;
-        long left = 1;
-        long right = (long)times[times.length-1] * n;
+        long left = 0;
+        long right = (long)times[times.length-1]*n;
+        long mid = (left+right)/2;
         while(left<=right){
-            long totalPeople = 0;
-            long mid = (left+right)/2;
-            for(int time: times){
-                totalPeople+= mid/time;
+            mid = (left+right)/2;
+            long people = 0; //int나누기 long이니까 long?
+            for(int t : times){
+                people += mid/t;
             }
-            if(totalPeople>=n){ //mid보다 더 짧은 시간 내 심사할 수도 있음
+            if(people>=n){
                 answer = mid;
                 right = mid-1;
-            }
-            else{
+            }else{
                 left = mid+1;
             }
         }
+            
+        
         return answer;
     }
 }
