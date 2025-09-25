05ade97897d506168931e48e64345ad5bc9d9957
eun9 <42036131+enjung@users.noreply.github.com>
2025-09-25 15:58:54 +0900

    [level 3] Title: 순위, Time: 10.58 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -4,22 +4,26 @@ class Solution {
     public int solution(int n, int[][] results) {
         int answer = 0;
         boolean[][] graph = new boolean[n+1][n+1];
-        for(int[]res : results){
+        for(int[] res : results){
             graph[res[0]][res[1]] = true;
         }
         for(int k=1;k<=n;k++){
             for(int i=1;i<=n;i++){
                 for(int j=1;j<=n;j++){
-                    if(graph[i][k]&&graph[k][j]) graph[i][j]=true;
+                    if(graph[i][k]&&graph[k][j]){
+                        graph[i][j]=true;
+                    }
                 }
             }
         }
         for(int i=1;i<=n;i++){
             int cnt = 0;
             for(int j=1;j<=n;j++){
-                if(graph[i][j] || graph[j][i]) cnt++;
+                if(graph[i][j]||graph[j][i]){
+                    cnt++;
+                }
             }
-            if(cnt==n-1) answer++;
+            if(cnt == n-1) answer++;
         }
         return answer;
     }
