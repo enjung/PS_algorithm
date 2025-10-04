9e911031fa35ced8b14132bb68b01780608ba50a
eun9 <42036131+enjung@users.noreply.github.com>
2025-10-04 17:16:46 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 35.60 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,43 +1,37 @@
 import java.util.*;
 
 class Solution {
-    public long solution(int[] diffs, int[] times, long limit) {
-        long answer = 0;
-        long left = 1;
-        long right = 100000;
-        long level = 1;
-        
+    public int solution(int[] diffs, int[] times, long limit) {
+        int answer = 0;
+        int left = 1;
+        int right = 100000;
         while(left<=right){
-            level = (left+right)/2;
-            long time = cal(level,diffs,times,limit);
-            
-            //level을 더 높여야 함
-            if(time>limit) {
-                left = level+1;
-            }
-            //level을 낮춰야 함
-            else{
+            int level = (left+right)/2;
+            long time = cal(diffs,times,level);
+            if(time<=limit){
+                //level을 더 낮춰도 가능성 있음
                 answer = level;
                 right = level-1;
+            }
+            else{
+                //level을 높여야 함
+                left = level+1;
             }
         }
         return answer;
     }
-    
-    public long cal(long level,int[] diffs, int[] times, long limit){
-            long cur=0;
-            for(int i=0;i<diffs.length;i++){
-                if(diffs[i]<=level){
-                    cur=cur+times[i];
-                    continue;
+    public long cal(int[] diffs, int[] times, int level){
+        long time = 0;
+        for(int i=0;i<diffs.length;i++){
+            if(diffs[i]<=level) time+=times[i];
+            else{
+                if(i==0) time+=(long)(diffs[i]-level)*times[i] + times[i];
+                else{
+                    time+=(long)(diffs[i]-level)*(times[i]+times[i-1])+times[i];
                 }
-                cur+=(diffs[i]-level)*(times[i]+times[i-1]) + times[i];
-                if (cur > limit) return cur;
             }
-            return cur;
         }
+        return time;
+    }
     
 }
-//이분탐색
-//첫번째 문제를 못 푸는 경우는 없나?
-//left=1이면 왜 index에러?
