017ada39e637cecd5409930f230321848daae0df
eun9 <42036131+enjung@users.noreply.github.com>
2025-11-11 00:00:58 +0900

    [level 3] Title: 정수 삼각형, Time: 9.28 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,28 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0;
-        int n = triangle.length;
-        int[][] dp = new int[n][n];
+        int[] dp = new int[triangle.length];
+        dp[0] = triangle[0][0];
         
-        //초기화
-        dp[0][0]=triangle[0][0];
-        
-        //dp채우기
-        for(int i=1;i<n;i++){
-            //첫번째 열
-            dp[i][0] = dp[i-1][0] + triangle[i][0];
-            //제일 마지막 열
-            dp[i][i] = dp[i-1][i-1] + triangle[i][i];
-            //양끝 경계를 제외한 가운데 값들
-            for(int j=1;j<i;j++){
-                dp[i][j]= Math.max(dp[i-1][j],dp[i-1][j-1])+triangle[i][j];
-            }
-            
+        for(int i=1; i<triangle.length;i++){
+            for(int j=i;j>=0;j--){
+              if(j==0) dp[j] = dp[j]+triangle[i][j];
+              else if(j==i) dp[j] = dp[j-1]+triangle[i][j];
+              else{
+                  dp[j] = Math.max(dp[j-1], dp[j]) + triangle[i][j];
+              }
+            }  
         }
         
-        //마지막 행에서 가장 dp가 제일 큰 값이 정답
-        for(int j=0; j<n; j++){
-            answer = Math.max(dp[n-1][j], answer);
+        int answer = dp[0];
+        for(int i=1;i<dp.length;i++){
+            answer = Math.max(answer, dp[i]);
         }
         return answer;
     }
