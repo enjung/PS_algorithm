5b03ee311890c2ac25b3e983e505482fe47275ae
eun9 <42036131+enjung@users.noreply.github.com>
2025-11-12 16:35:48 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,28 +1,23 @@
 import java.util.*;
 
 class Solution {
+    int cnt = 0;
+    boolean[] visited;
     public int solution(int n, int[][] computers) {
-        
-        boolean visited[] = new boolean[n];
-        int answer = 0;
-        
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(computers,visited,n,i);
-                answer++; 
-                //dfs안에서 answer++하는게 아니라, dfs가 끝나야 하나의 덩어리가 끝난것이므로 dfs 호출 끝나면 증가
+        visited = new boolean[n];
+        for(int i=0; i<n;i++){
+            if(!visited[i]) {
+                dfs(n,i,computers);
+                cnt++;
             }
         }
-        return answer;
+        return cnt;
     }
-    
-    public void dfs(int[][] computers, boolean[] visited,int n, int i){
-        //dfs호출 조건에 방문여부를 이미 확인했으므로 바로 방문함으로 체크
+    public void dfs(int n, int i, int[][] computers){
         visited[i]=true;
         for(int j=0;j<n;j++){
-            if(!visited[j]&&computers[i][j]==1){
-                dfs(computers,visited,n,j);
-            }
+            //i==j여도 어차피 visited 조건으로 걸러짐
+            if(computers[i][j]==1 && !visited[j]) dfs(n,j,computers);
         }
     }
 }
